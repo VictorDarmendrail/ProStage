7b5b10e5d4bc39f4c7f2fecd8fa868e34f0c87da
Victor Darmendrail <vdarmendrail@iutbayonne.univ-pau.fr>
2022-01-05 10:56:14 +0100

    controller accueil,formation,entreprise,descritifStage

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -9,12 +9,35 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProStageController extends AbstractController
 {
     /**
-     * @Route("/pro/stage", name="pro_stage")
+     * @Route("/", name="ProStage")
      */
     public function index(): Response
     {
-        return $this->render('pro_stage/index.html.twig', [
-            'controller_name' => 'ProStageController',
-        ]);
+        return $this->render('pro_stage/index.html.twig');
+    }
+
+    /**
+     * @Route("/entreprises", name="entreprises")
+     */
+    public function afficherEntreprises(): Response
+    {
+        return $this->render('pro_stage/afficherEntreprises.html.twig');
+    }
+
+    /**
+     * @Route("/Formations", name="formations")
+     */
+    public function afficherFormations(): Response
+    {
+        return $this->render('pro_stage/afficherFormations.html.twig');
+    }
+    
+    /**
+     * @Route("/stage/{id}", name="descriptionStage")
+     */
+    public function afficherDescriptifStage($id): Response
+    {
+        return $this->render('pro_stage/afficherDescriptifStage.html.twig',
+      ['idStage' => $id]);
     }
 }
